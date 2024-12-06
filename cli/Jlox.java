03220b0f2683cc4befb1375e6e311428c2b9ebf0
HuyDNA <huydo862003@gmail.com>
2024-12-06 15:58:57 +0700

    feat: basic jlox cli

---
+++ cli/Jlox.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Jlox {
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64); // EX_USAGE sysexits code
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  public static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+  }
+
+  public static void runPrompt() throws IOException {
+    BufferedReader reader = new BUfferredReader(new InputStreamReader(System.in));
+
+    while (true) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line);
+    }
+  }
+
+  private static void run(String source) {
+    // TODO
+  }
+}
