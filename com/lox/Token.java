699d977e241c70781e54affa06ba344053f71f10
HuyDNA <huydo862003@gmail.com>
2024-12-09 12:35:45 +0700

    feat: add Token class

---
+++ com/lox/Token.java
@@ -0,0 +1,18 @@
+package com.lox;
+
+class Token {
+  final TokenType type;
+  final String lexeme;
+  final Object literal; // value of `lexeme` interpreted as `type` in the program
+
+  final int startOffset;
+  final int endOffset;
+
+  Token(TokenType type, String lexeme, Object literal, int startOffset, int endOffset) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.startOffset = startOffset;
+    this.endOffset = endOffset;
+  }
+}
