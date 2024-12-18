3eedcee6e0a531d95c5bff5aab0cfab3c97c8a7a
HuyDNA <huydo862003@gmail.com>
2024-12-18 22:32:59 +0700

    feat: add expression pretty printer

---
+++ com/lox/PrettyPrinter.java
@@ -0,0 +1,13 @@
+package com.lox;
+
+public class PrettyPrinter {
+  public String printExpr(Expr expr) {
+    return switch (expr) {
+      case Expr.Unary u -> String.format("(%s %s)", u.op, this.printExpr(u.inner));
+      case Expr.Binary b -> String.format("(%s %s %s)", b.op, this.printExpr(b.left), this.printExpr(b.right));
+      case Expr.Literal l -> l.value.lexeme;
+      case Expr.Grouping g -> String.format("(group %s)", this.printExpr(g.inner));
+      default -> throw new Error("Non-exhaustive check");
+    };
+  }
+}
