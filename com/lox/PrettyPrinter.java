1b10cdd5c1134fe3a477366c2c1ec223488b6eb0
HuyDNA <huydo862003@gmail.com>
2024-12-20 22:51:18 +0700

    fix: print lexeme of op in prettyPrinter

--- com/lox/PrettyPrinter.java
+++ com/lox/PrettyPrinter.java
@@ -5,8 +5,8 @@ import com.lox.ast.Expr;
 public class PrettyPrinter {
   public String printExpr(Expr expr) {
     return switch (expr) {
-      case Expr.Unary u -> String.format("(%s %s)", u.op, this.printExpr(u.inner));
-      case Expr.Binary b -> String.format("(%s %s %s)", b.op, this.printExpr(b.left), this.printExpr(b.right));
+      case Expr.Unary u -> String.format("(%s %s)", u.op.lexeme, this.printExpr(u.inner));
+      case Expr.Binary b -> String.format("(%s %s %s)", b.op.lexeme, this.printExpr(b.left), this.printExpr(b.right));
       case Expr.Literal l -> l.value.lexeme;
       case Expr.Grouping g -> String.format("(group %s)", this.printExpr(g.inner));
       case Expr.Variable v -> String.format("%s", v.var.lexeme);
