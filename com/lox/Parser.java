08c096614cc7f6ae49acbf5e1abd5ee495cda5b0
HuyDNA <huydo862003@gmail.com>
2024-12-20 18:28:21 +0700

    feat: add basic parse method to Parser

--- com/lox/Parser.java
+++ com/lox/Parser.java
@@ -1,5 +1,29 @@
 package com.lox;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.lox.ast.Program;
+import com.lox.ast.Stmt;
+import com.lox.ast.Token;
+import com.lox.utils.Pair;
+
 public class Parser {
-  
+  private final List<Token> tokens;
+  private int currentOffset = 0;
+  private Program program = new Program();
+  private List<ParserException> errors = new ArrayList<>();
+
+  public Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+
+  public Pair<Program, List<ParserException>> parse() throws ParserException {
+    if (this.isAtEnd()) return new Pair<>(this.program, this.errors);
+    return new Pair<>(this.program, this.errors);
+  }
+
+  private boolean isAtEnd() {
+    return this.currentOffset == tokens.size();
+  }
 }
