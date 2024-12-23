a7518a710d70c385966c65d0aeb4cde2a6acbd20
HuyDNA <huydo862003@gmail.com>
2024-12-23 22:05:18 +0700

    feat: add DeclStmt class

--- jlox/com/lox/ast/Stmt.java
+++ jlox/com/lox/ast/Stmt.java
@@ -16,4 +16,14 @@ public abstract class Stmt extends SyntaxNode {
       this.expr = expr;
     }
   }
+
+  public static class DeclStmt extends Stmt {
+    public final Token id;
+    public final Expr expr;
+
+    public DeclStmt(Token id, Expr expr) {
+      this.id = id;
+      this.expr = expr;
+    }
+  }
 }
