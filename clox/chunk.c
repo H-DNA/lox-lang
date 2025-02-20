fd683c73ddd6830abf924f6107b73d3c8efa95af
HuyDNA <huydo862003@gmail.com>
2025-02-20 16:27:15 +0700

    feat: include line information in chunk

--- clox/chunk.c
+++ clox/chunk.c
@@ -7,38 +7,43 @@
 void initChunk(Chunk *chunk) {
   chunk->count = 0;
   chunk->capacity = 8;
+
   chunk->code = (uint8_t *)malloc(chunk->capacity * sizeof(uint8_t));
-  initValueArray(&chunk->constants);
-  if (chunk->code == NULL) {
+  chunk->lines = (unsigned int *)malloc(chunk->capacity * sizeof(unsigned int));
+  if (chunk->code == NULL || chunk->lines == NULL) {
     fprintf(stderr, "Failed to malloc memory in initChunk with capacity 8");
     exit(1);
   }
+
+  initValueArray(&chunk->constants);
 }
 
-void writeChunk(Chunk *chunk, uint8_t byte) {
+void writeChunk(Chunk *chunk, uint8_t byte, unsigned int line) {
   if (chunk->count == chunk->capacity) {
     int new_capacity = chunk->capacity * 2;
     chunk->capacity = new_capacity;
     chunk->code = realloc(chunk->code, new_capacity);
-    if (chunk->code == NULL) {
+    chunk->lines = realloc(chunk->lines, new_capacity);
+    if (chunk->code == NULL || chunk->lines == NULL) {
       fprintf(stderr, "Failed to realloc memory in writeChunk with capacity %d",
               (int)new_capacity);
       exit(1);
     }
   }
-  chunk->code[chunk->count++] = byte;
+  chunk->code[chunk->count] = byte;
+  chunk->lines[chunk->count] = line;
+  ++chunk->count;
 }
 
 void freeChunk(Chunk *chunk) {
   free(chunk->code);
+  free(chunk->lines);
   freeValueArray(&chunk->constants);
 }
 
-int addConstant(Chunk* chunk, Value value) {
+int addConstant(Chunk *chunk, Value value) {
   writeValueArray(&chunk->constants, value);
   return chunk->constants.count - 1;
 }
 
-void printValue(Value value) {
-  printf("%g", value);
-}
+void printValue(Value value) { printf("%g", value); }
--- clox/main.c
+++ clox/main.c
@@ -6,9 +6,9 @@ int main(int argc, const char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
   int index = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT);
-  writeChunk(&chunk, index);
-  writeChunk(&chunk, OP_RETURN);
+  writeChunk(&chunk, OP_CONSTANT, 0);
+  writeChunk(&chunk, index, 0);
+  writeChunk(&chunk, OP_RETURN, 0);
   disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
   return 0;
