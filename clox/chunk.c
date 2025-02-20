1d51d24232c2085f816d2da09a89668f3b9e9b52
HuyDNA <huydo862003@gmail.com>
2025-02-20 16:06:11 +0700

    feat: incorporate constants value array into chunk

--- clox/chunk.c
+++ clox/chunk.c
@@ -1,4 +1,5 @@
 #include "chunk.h"
+#include "value.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,6 +8,7 @@ void initChunk(Chunk *chunk) {
   chunk->count = 0;
   chunk->capacity = 8;
   chunk->code = (uint8_t *)malloc(chunk->capacity * sizeof(uint8_t));
+  initValueArray(&chunk->constants);
   if (chunk->code == NULL) {
     fprintf(stderr, "Failed to malloc memory in initChunk with capacity 8");
     exit(1);
@@ -27,4 +29,7 @@ void writeChunk(Chunk *chunk, uint8_t byte) {
   chunk->code[chunk->count++] = byte;
 }
 
-void freeChunk(Chunk *chunk) { free(chunk->code); }
+void freeChunk(Chunk *chunk) {
+  free(chunk->code);
+  freeValueArray(&chunk->constants);
+}
