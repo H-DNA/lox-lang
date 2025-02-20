b1bf8c933704fa73212bb39e37cf8efd192c0761
HuyDNA <huydo862003@gmail.com>
2025-02-21 00:13:36 +0700

    feat: add conditional debug macro to disassemble instruction

--- clox/vm.c
+++ clox/vm.c
@@ -1,5 +1,6 @@
 #include "vm.h"
 #include "chunk.h"
+#include "debug.h"
 #include <stdio.h>
 
 void initVM(VirtualMachine *vm) {
@@ -15,6 +16,9 @@ InterpretResult interpret(VirtualMachine *vm) {
   (vm->ip += 2, (vm->chunk.code[vm->ip - 2] << 8) + vm->chunk.code[vm->ip - 1])
 #define READ_CONSTANT(id) (vm->chunk.constants.values[(id)])
   while (true) {
+#ifdef DEBUG_TRACE_EXECUTION
+    disassembleInstruction(&vm->chunk, vm->ip);
+#endif
     uint8_t instruction = READ_BYTE();
     switch (instruction) {
     case OP_RETURN:
