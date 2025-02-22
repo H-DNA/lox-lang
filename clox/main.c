185283f2ca5fb6ce8f0ad07fbadcf7af7981a5bf
HuyDNA <huydo862003@gmail.com>
2025-02-22 18:26:16 +0700

    feat: add runFile and repl

--- clox/main.c
+++ clox/main.c
@@ -1,17 +1,80 @@
-#include "chunk.h"
 #include "common.h"
-#include "debug.h"
 #include "vm.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static void repl();
+static void runFile(const char *pathname);
+static char *readFile(const char *pathname);
 
 int main(int argc, const char *argv[]) {
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runFile(argv[1]);
+  } else {
+    fprintf(stderr, "Usage: clox [path]\n");
+    exit(2);
+  }
+  return 0;
+}
+
+static void repl() {
   VirtualMachine vm;
   initVM(&vm);
-  for (int i = 0; i < 300; ++i) {
-    writeConstant(&vm.chunk, i, 0);
+
+  char line[1024];
+  while (true) {
+    printf("> ");
+
+    if (!fgets(line, sizeof(line), stdin)) {
+      printf("\n");
+      break;
+    }
   }
-  writeChunk(&vm.chunk, OP_NEGATE, 0);
-  writeChunk(&vm.chunk, OP_RETURN, 0);
-  interpret(&vm);
+
   freeVM(&vm);
-  return 0;
+}
+
+static void runFile(const char *pathname) {
+  VirtualMachine vm;
+  initVM(&vm);
+
+  char *source = readFile(pathname);
+  InterpretResult result = interpret(&vm);
+  free(source);
+
+  freeVM(&vm);
+
+  if (result != INTERPRET_OK) {
+    exit(1);
+  }
+}
+
+static char *readFile(const char *pathname) {
+  FILE *file = fopen(pathname, "rb");
+  if (!file) {
+    fprintf(stderr, "Failed to open file %s", pathname);
+    exit(1);
+  }
+
+  fseek(file, 0L, SEEK_END);
+  const size_t fileSize = ftell(file);
+  rewind(file);
+
+  char *buffer = (char *)malloc(fileSize + 1);
+  if (!buffer) {
+    fprintf(stderr, "Failed to malloc memory for file content %s", pathname);
+    exit(1);
+  }
+
+  const size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+  if (bytesRead != fileSize) {
+    fprintf(stderr, "Failed to read file content %s", pathname);
+    exit(1);
+  }
+  buffer[bytesRead] = '\0';
+
+  fclose(file);
+  return buffer;
 }
